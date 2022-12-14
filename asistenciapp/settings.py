836fe6d9e8200285a0d09d4b228dc72581cc6fb6
Nandocho8 <ccontrerasvergara@hotmail.cl>
2022-12-14 18:41:13 -0300

    req add library djdatabase

--- asistenciapp/settings.py
+++ asistenciapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+SECRET_KEY = SECRET_KEY = os.environ.get(
+    'SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -30,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:    
+if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
@@ -87,10 +89,10 @@ WSGI_APPLICATION = 'asistenciapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(        
-        # Feel free to alter this value to suit your needs.        
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600    )
+        conn_max_age=600)
 }
 
 
